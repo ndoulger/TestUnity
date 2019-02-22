525bc3bf4fac332f37275f276f984708d1d3b049
ndoulger <47592069+ndoulger@users.noreply.github.com>
2019-02-22 18:37:52 +0200

    ChangingRotationInfosVideo

---
+++ GardenTourPrj/Assets/ShowFirstInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowFirstInfo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("Info1").SetActive(true);
+        GameObject.Find("Info2").SetActive(false);
+
+        GameObject.Find("btnNext").SetActive(true);
+        GameObject.Find("btnPrevius").SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
