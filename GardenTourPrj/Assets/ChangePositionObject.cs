0b3552c7a5d2341a7baab41d427fd1161d4ec453
ndoulger <47592069+ndoulger@users.noreply.github.com>
2019-02-24 00:21:08 +0200

    tryingPlantSeed

---
+++ GardenTourPrj/Assets/ChangePositionObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePositionObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MoveTo(GameObject aGameObject)
+    {
+        transform.position = aGameObject.transform.position;
+        //  GameObject.Find("PlantA").transform.position.Set(GameObject.Find("SeedA").transform.position);
+    }
+}
---
+++ GardenTourPrj/Assets/StartPlantSeedScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPlantSeedScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("SeedDaisy").SetActive(true);
+        GameObject.Find("SeedOrtansia").SetActive(true);
+        GameObject.Find("SeedSnowDrop").SetActive(true);
+        GameObject.Find("Daisy").SetActive(false);
+        GameObject.Find("ortansia").SetActive(false);
+        GameObject.Find("SnowDrop").SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
