3d6ce211a7a9de99fb9e6ecd048106f1853ea7c2
ndoulger <47592069+ndoulger@users.noreply.github.com>
2019-02-24 18:44:13 +0200

    PlayPauseButtons

---
+++ GardenTourPrj/Assets/FirstVideoScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstVideoScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("btnPause").SetActive(true);
+        GameObject.Find("btnPlay").SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
