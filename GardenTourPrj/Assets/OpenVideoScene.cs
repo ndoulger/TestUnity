c5bea6c197d3ece263ef6f82915282afc4965b4a
ndoulger <47592069+ndoulger@users.noreply.github.com>
2019-02-27 10:56:21 +0200

    ChangeMenutoButtons

---
+++ GardenTourPrj/Assets/OpenVideoScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenVideoScene : MonoBehaviour
+{
+    public void OpenVideoSnc()
+    {
+        SceneManager.LoadScene("VideoScene");
+    }
+}
