30e82f7cb0b34df05d223ca77aa6e68005af3ea2
ndoulger <47592069+ndoulger@users.noreply.github.com>
2019-02-28 11:13:33 +0200

    UpdatebuttonsAndImages

--- GardenTourPrj/Assets/StartPlantSeedScene.cs
+++ GardenTourPrj/Assets/StartPlantSeedScene.cs
@@ -7,10 +7,6 @@ public class StartPlantSeedScene : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Find("SeedDaisy").SetActive(true);
-
-        GameObject.Find("SeedOrtansia").SetActive(true);
-        GameObject.Find("SeedSnowDrop").SetActive(true);
         GameObject.Find("Daisy").SetActive(false);
         GameObject.Find("ortansia").SetActive(false);
         GameObject.Find("SnowDrop").SetActive(false);
